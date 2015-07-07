619ef95574da2d69124b981dd9f96b6b349de257
gprinziv <Gprinziv@gmail.com>
2015-07-07 06:27:12 +0300

    Finished closest_pair
    Pair finding algorithm complete (I think). Not as rigorously tested as I'd prefer but I'm losing steam to write test cases. Should work for infinitely many dimensions and pairs, limited by processing power.

--- Python/Algorithms/closestPair.py
+++ Python/Algorithms/closestPair.py
@@ -1,5 +1,8 @@
 """closestPair.py - A script to find the two closest points in metric space
-by Giovanni Prinzivalli"""
+   by Giovanni Prinzivalli"""
+
+from itertools import combinations
+from math import sqrt
 
 def closest_pair(coordinates):
     """Takes a list of tuples of identical size as input.
@@ -8,15 +11,38 @@ def closest_pair(coordinates):
         if len(coordinates) < 2:
             raise ValueError("Must have at least two sets of coordinates to find the closest pair.")
 
-        for vals in coordinates:
-            if len(vals) != len(coordinates[0]):
+        shortest = (coordinates[0], coordinates[1])
+        shdist = find_dist(shortest)
+        for pair in combinations(coordinates, 2):
+            if len(pair[0]) != len(pair[1]):
                 raise ValueError("All coordinate pairs must be of the same size.")
 
-        #Find n-dimensional Pythagorean value for each pair of coordinates. We'll cap it at 3 dimensions for sanity's sake.
-        #Log the smallest pair.
-        pass
+            distance = find_dist(pair)
+            if distance < shdist:
+                shortest = pair
+                shdist = distance
+
+        return shortest
+
     except TypeError:
-        print "Non-number coordinates can't be calculated.
+        print "Non-number coordinates can't be calculated."
+
+def find_dist(pair):
+    """Subfunction to find the distance between two tuples of coordinates.
+       Assumes a and b have the same size"""
+    a = pair[0]
+    b = pair[1]
+    dist = 0.00
+    for i in range(0, len(a)):
+        dist += pow(a[i] - b[i], 2)
+
+    return sqrt(dist)
 
 if __name__ == "__main__":
-    pass
+    print "closestPair.py testing. I got lazy on actually trying this."
+    point_a = (0, 0)
+    point_b = (4, 4)
+    point_c = (3, 4)
+    point_d = (2, 2)
+
+    print closest_pair([point_a, point_b, point_c, point_d])
