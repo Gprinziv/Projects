eacd82a0df39f5c950c7d37aa88a7865abf24b1c
gprinziv <Gprinziv@gmail.com>
2015-07-08 04:02:02 +0300

    Started String Operations
    Completed the most basic string operation methods.

---
+++ Python/Text/stringOps.py
@@ -0,0 +1,50 @@
+"""stringOps.py - Various String manipulation operations
+   by Giovanni Prinzivalli """
+
+def fizz_buzz(length):
+    """Prints all numbers between 1 and length, replacing multiples
+       of 3 with Fizz, multiples of 5 with Buzz, and multiples of 15 with FizzBuzz"""
+    arr = []
+    for i in range(1, length + 1):
+        if not i % 15:
+            arr.append("FizzBuzz")
+        elif not i % 3:
+            arr.append("Fizz")
+        elif not i % 5:
+            arr.append("Buzz")
+        else:
+            arr.append(i)
+    return arr
+
+def reverse_string(text):
+    """Reverses a string. Not rocket surgery."""
+    return text[::-1]
+
+def pig_latin(text):
+    """Later."""
+    pass
+
+def is_palindrome(text):
+    """Checks if a string reads the same both ways"""
+    return text.lower() == text[::-1].lower()
+
+def count_words(text):
+    """Counts the number of words in a string. For added complexity, give a summary and read from a file."""
+    pass
+
+if __name__ == "__main__":
+    print "Fizzbuzz list..."
+    print fizz_buzz(100)
+
+    print "String reverser"
+    print reverse_string("Hello!")
+
+    print "Pig latin converter..."
+    print "Psyche. Implementing later."
+
+    print "Palindrome checking..."
+    print is_palindrome("aha")
+    print is_palindrome("racecar")
+    print is_palindrome("aHA")
+    print is_palindrome("Racecar")
+    print is_palindrome("Magic schoolbus")
--- README.md
+++ README.md
@@ -93,7 +93,7 @@ X**Collatz Conjecture** - Start with a number *n > 1*. Find the number of steps 
 
 X**Sorting** - Implement two types of sorting algorithms: Merge sort and bubble sort.
 
-**Closest pair problem** - The closest pair of points problem or closest pair problem is a problem of computational geometry: given *n* points in metric space, find a pair of points with the smallest distance between them.
+X**Closest pair problem** - The closest pair of points problem or closest pair problem is a problem of computational geometry: given *n* points in metric space, find a pair of points with the smallest distance between them.
 
 X**Sieve of Eratosthenes** - The sieve of Eratosthenes is one of the most efficient ways to find all of the smaller primes (below 10 million or so).
 
